c61a2ab693db917efd43bd6725dccd57f669244f
thienthu1410student <1312582@student.hcmus.edu.vn>
2016-05-11 17:20:15 +0700

    get the value of numpad button, sign button and display it in the text field

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,44 +1,6 @@
-// Plugin options and our code
+function getCharacter(button)
+{
+	var txtFieldValue = document.getElementById("txtFieldString").value;
+	document.getElementById("txtFieldString").value = txtFieldValue + button.value;
+}
 
-
-$(document).ready(function(){
-	$("#modal_trigger").leanModal({
-		top: 100,
-		overlay: 0.6,
-		closeButton: ".modal_close"
-	});
-
-	$(function() {
-		// Calling Login Form
-		$("#login_form").click(function() {
-			$(".social_login").hide();
-			$(".user_login").show();
-			return false;
-		});
-
-		// Calling Register Form
-		$("#register_form").click(function() {
-			$(".social_login").hide();
-			$(".user_register").show();
-			$(".header_title").text('Register');
-			return false;
-		});
-
-		// Going back to Social Forms
-		$(".back_btn").click(function() {
-			$(".user_login").hide();
-			$(".user_register").hide();
-			$(".social_login").show();
-			$(".header_title").text('Login');
-			return false;
-		});
-	});
-
-	$("#login_btn").click(function() {
-		register('pig', 'pig', 'Pig');
-	})
-
-	$("#reg_btn").click(function() {
-		alert("Register");
-	})
-});
