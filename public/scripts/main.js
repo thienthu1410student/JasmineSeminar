f830e533e06fd4fa2fd59fa92801cd7e182c2b36
thienthu1410student <1312582@student.hcmus.edu.vn>
2016-05-12 08:36:43 +0700

    add jasmine library, modify the function "getCharacter (button)" & "clearAll()"

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,14 +1,10 @@
 function getCharacter(button)
 {
-	var txtFieldValue = document.getElementById("txtFieldString").value;
-
-	if (txtFieldValue!==" ")
-		document.getElementById("txtFieldString").value = txtFieldValue + button.innerHTML;
-	else
-		document.getElementById("txtFieldString").value = button.innerHTML;
+	txtFieldString.value = txtFieldString.value + button.innerHTML;
 }
 
 function clearAll()
 {
-	document.getElementById("txtFieldString").value = " ";
+	document.getElementById("txtFieldString").value = "";
 }
+
