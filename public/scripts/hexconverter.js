f8b904788cd9ad89dfb0a26c6990bfb7118e84ad
Duong Tien <dtien147@gmail.com>
2016-05-16 01:28:02 +0700

    Hex converter

---
+++ public/scripts/hexconverter.js
@@ -0,0 +1,47 @@
+function toHex() {
+	var hexString;
+	calculate(document.getElementById('txtFieldString').value);
+	switch(base) {
+		case 2:
+			hexString = binToHex(calResult.toString());
+			break;
+		case 10:
+			hexString = decToHex(calResult);
+			break;	
+		case 16:
+			hexString = txtFieldString.value;		
+			break;
+	}
+
+	base = 16;
+	txtFieldString.value = hexString;
+	calResult = hexString;
+}
+
+function binToHex(s) {
+    var i, k, part, accum, ret = '';
+    for (i = s.length-1; i >= 3; i -= 4) {
+        part = s.substr(i+1-4, 4);
+        accum = 0;
+        for (k = 0; k < 4; k += 1) {
+            accum = accum * 2 + parseInt(part[k], 10);
+        }
+        if (accum >= 10) {
+            ret = String.fromCharCode(accum - 10 + 'A'.charCodeAt(0)) + ret;
+        } else {
+            ret = String(accum) + ret;
+        }
+    }
+    if (i >= 0) {
+        accum = 0;
+        for (k = 0; k <= i; k += 1) {
+            accum = accum * 2 + parseInt(s[k], 10);
+        }
+        ret = String(accum) + ret;
+    }
+    return ret;
+}
+
+function decToHex(n) {
+	return n.toString(16);	
+}
