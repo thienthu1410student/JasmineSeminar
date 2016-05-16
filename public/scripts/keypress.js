05c4380b51e1ed3bba06b30ed2b62b731cd3e392
Duong Tien <dtien147@gmail.com>
2016-05-16 19:21:07 +0700

    Add hot key

--- public/scripts/keypress.js
+++ public/scripts/keypress.js
@@ -1,32 +1,64 @@
-function isNumber(charCode) {
-	var chr = String.fromCharCode(charCode);
-	chr = chr.toLowerCase();
+function isNumber(chr) {
 	return ('0' <= chr && chr <= '9');
 }
 
-function isInputHexChar(charCode) {
+function isInputHexChar(chr) {
 	if(base != 16)
 		return false;
 
-	var chr = String.fromCharCode(charCode);
 	chr = chr.toUpperCase();
 	return ('A' <= chr && chr <= 'F');
 }
 
-function isOperant(charCode) {
-	var chr = String.fromCharCode(charCode);
-	chr = chr.toLowerCase();
+function isOperant(chr) {
 	return (chr == '+' || chr == '-' || chr == '*' || chr == '/');
 }
 
-function isValidate(charCode) {
-	return (isNumber(charCode) || isInputHexChar(charCode) || isOperant(charCode));	
+function isAssign(charCode) {
+	return (charCode === 61 || charCode === 13);
+}
+
+function isClear(charCode) {
+	//Delete in chrome = 127, firefox = 0
+	return (charCode === 0 || charCode === 127);
+}
+
+function isHotkey(charCode) {
+	if(isClear(charCode)) {
+		clear();
+	}
+	else if(isAssign(charCode)) {
+		assign();
+	}
+	else return false;
+
+	return true;
+}
+
+function clear() {
+	var str = txtFieldString.value;
+	txtFieldString.value = str.substring(0, Math.max(str.length - 1, 0));
+}
+
+function clearAll() {
+	txtFieldString.value = "";
+}
+
+function assign() {
+	calculate(txtFieldString.value);
+}
+
+function isValidate(chr) {	
+	return (isNumber(chr) || isInputHexChar(chr) || isOperant(chr));	
 }
 
 $(document).keypress(function(key) {
-    if(isValidate(key.which)) {
-    	txtFieldString.value += String.fromCharCode(key.which).toUpperCase();
-    }
+	if(! isHotkey(key.which)) {
+		var chr = String.fromCharCode(key.which);
+	    if(isValidate(chr)) {    	
+	    	txtFieldString.value += chr.toUpperCase();
+	    }
+	}
 });
 
 $(document).ready(function() { 
