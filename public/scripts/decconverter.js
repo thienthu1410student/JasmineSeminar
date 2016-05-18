9a840096a34855903a0c59433bd831c51462d067
Nguyen Hoang Tin <hoangtin1514@gmail.com>
2016-05-18 19:55:52 +0700

    Binary caculator

---
+++ public/scripts/decconverter.js
@@ -0,0 +1,32 @@
+function toDec() {
+	var decString;
+	calculate(document.getElementById('txtFieldString').value);
+	switch(base) {
+		case 2:
+			decString = binToDec(txtFieldString.value);
+			break;
+		case 10:
+			decString = txtFieldString.value;		
+			break;	
+		case 16:
+			decString = hexToDec(txtFieldString.value);
+			break;
+	}
+
+	base = 10;
+	txtFieldString.value = decString;
+}
+
+function binToDec(s) {
+	if(isBinary(s) == false)
+		return "Is not binary";
+  
+    return parseInt(s, 2);
+}
+
+function hexToDec(s) {
+	if(isHex(s.toString()) == false)
+		return "Is not decimal";
+
+    return parseInt(s, 16);
+}
